b5a6c12847c003cc141881adeb9107f473ae4966
flatrov <flatrov1@gmail.com>
2026-08-09 22:58:00 +0330

    fix: responsive header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,18 +5,18 @@ export function Header() {
   return (
     <>
       <header className="sticky z-40 inset-0 w-full bg-background dark:shadow-white/5 shadow-sm">
-        <div className="max-w-6xl mx-auto flex justify-between items-center py-2">
+        <div className="container flex justify-between items-center py-2">
           <div className="flex items-center gap-2">
             <Link to="/">
               <img src="/logo.svg" alt="logo" width="50" />
             </Link>
 
-            <div className="min-w-px min-h-4 bg-border">h</div>
+            <div className="min-w-px min-h-4 bg-border"></div>
 
             <Link to="#">
               <div className="flex items-center gap-2 p-3 rounded-md hover:bg-gray-100 hover:text-primary dark:hover:bg-white/10 dark:hover:text-white transition-colors duration-200">
                 <LocationEdit size={18} />
-                <span className="text-sm font-medium">انتخاب شهر</span>
+                <span className="text-sm font-medium max-lg:hidden">انتخاب شهر</span>
               </div>
             </Link>
           </div>
@@ -29,7 +29,7 @@ export function Header() {
               </div>
             </Link>
 
-            <Link to="#">
+            <Link to="#" className="max-lg:hidden">
               <div className="flex items-center gap-2 p-3  rounded-md hover:bg-gray-100 hover:text-primary dark:hover:bg-white/10 dark:hover:text-white transition-colors duration-200">
                 <HelpCircle size={18} />
                 <span className="text-sm font-medium">پشتیبانی</span>
