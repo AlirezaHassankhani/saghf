070fdd832ea4b4d72ad4d7e727f1411e9d453ff7
flatrov <flatrov1@gmail.com>
2026-08-09 23:08:52 +0330

    fix: responsive footer component

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -11,17 +11,24 @@ export function Footer() {
   return (
     <>
       <footer className="border-t border-border py-4">
-        <div className="max-w-6xl mx-auto flex justify-between items-center">
-          <nav className="flex text-sm font-medium divide-x divide-foreground/30">
-            {navItems.map((item) => (
-              <Link to={item.to} className="hover:text-primary transition-colors duration-200 px-4">{item.title}</Link>
-            ))}
-          </nav>
+        <div className="container">
+          <div className="flex flex-wrap gap-x-4 gap-y-6 justify-center md:justify-between items-center">
+            <nav className="max-md:w-full flex justify-between text-xs sm:text-sm font-medium md:divide-x divide-foreground/30">
+              {navItems.map((item) => (
+                <Link
+                  to={item.to}
+                  className="hover:text-primary transition-colors duration-200 md:px-4"
+                >
+                  {item.title}
+                </Link>
+              ))}
+            </nav>
 
-          <div className="flex justify-between gap-4">
-            <img src="/1.png" alt="test" width="40" />
-            <img src="/2.png" alt="test" width="40" />
-            <img src="/3.png" alt="test" width="40" />
+            <div className="flex justify-between gap-4">
+              <img src="/1.png" alt="test" width="40" />
+              <img src="/2.png" alt="test" width="40" />
+              <img src="/3.png" alt="test" width="40" />
+            </div>
           </div>
         </div>
       </footer>
