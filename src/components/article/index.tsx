5675f18ac081de4a3fb177a4568e46d1eb73e036
flatrov <flatrov1@gmail.com>
2026-08-09 23:18:34 +0330

    style: article box

--- src/components/article/index.tsx
+++ src/components/article/index.tsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 export function Article() {
   return (
-    <article className="border border-border rounded-md p-4 space-y-2.5">
+    <article className="border border-border rounded-md py-5 px-4 space-y-5">
       <p className="font-medium">چرا با اینکه پرداخت را انجام داده ام، خدمتی دریافت نکرده ام؟</p>
 
-      <p className="text-justify text-foreground">
+      <p className="text-justify text-foreground line-clamp-2">
         اگر با وجود پرداخت هزینه، خدمتی دریافت نکردید، در صورت پرداخت هزینه از
         طریق درگاه بانکی موضوع را از طریق پشتیبانی بانک پیگیری کنید اما در صورتی
         که پرداخت با استفاده از درگاه کافه بازار انجام شده با پشتیبانی بازار
@@ -14,9 +14,9 @@ export function Article() {
       </p>
 
       <Link to="#" className="inline-block">
-        <div className="flex items-center gap-2 text-primary">
+        <div className="flex items-center gap-2 text-primary hover:text-primary/90 transition-colors duration-200 group">
           ادامه مقاله
-          <ArrowLeft size={16} />
+          <ArrowLeft size={16} className="group-hover:-translate-x-1 transition-transform duration-200" />
         </div>
       </Link>
     </article>
