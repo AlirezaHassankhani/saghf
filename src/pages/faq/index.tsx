a277175d253589fc5ffe67657c5a4f533d5f578a
flatrov <flatrov1@gmail.com>
2026-08-09 18:01:27 +0330

    style: dark mode for faq  page

--- src/pages/faq/index.tsx
+++ src/pages/faq/index.tsx
@@ -9,53 +9,55 @@ import "swiper/css";
 export function Faq() {
   return (
     <>
-      <section className="relative bg-primary mb-10">
-        <div className="space-y-5 max-w-2xl mx-auto py-6">
-          <div className="flex items-center gap-2 bg-white/20 rounded-full text-white w-fit px-3 py-1">
-            <Stars size={12} />
-            <span className="font-semibold text-xs">پشتیبانی هوشمند سقف</span>
-          </div>
-
-          <div>
-            <h3 className="text-white font-semibold text-3xl mb-2">
-              چطور می‌تونم کمکت کنم؟
-            </h3>
-            <p className="text-muted text-sm">
-              در چند ثانیه پاسخ هر سوالی که داری رو می‌گیری!
-            </p>
-          </div>
-
-          <div className="flex items-center bg-white w-full rounded-xl">
-            <input
-              type="text"
-              className="w-full outline-none py-6 pr-4 font-medium placeholder:text-foreground"
-              placeholder="هر سوالی داری بپرس ..."
-            />
-            <div className="px-4">
-              <button
-                type="button"
-                className="size-10 rounded-full bg-gray-200 text-foreground flex items-center justify-center cursor-pointer"
-              >
-                <ChevronLeft size={16} />
-              </button>
+      <section className="bg-primary dark:bg-red-900 mb-10">
+        <div className="relative max-w-6xl mx-auto">
+          <div className="space-y-5 max-w-2xl mx-auto py-6">
+            <div className="flex items-center gap-2 bg-white/20 rounded-full text-white w-fit px-3 py-1">
+              <Stars size={12} />
+              <span className="font-semibold text-xs">پشتیبانی هوشمند سقف</span>
             </div>
+
+            <div>
+              <h3 className="text-white font-semibold text-3xl mb-2">
+                چطور می‌تونم کمکت کنم؟
+              </h3>
+              <p className="text-muted text-sm">
+                در چند ثانیه پاسخ هر سوالی که داری رو می‌گیری!
+              </p>
+            </div>
+
+            <div className="flex items-center bg-white w-full rounded-xl">
+              <input
+                type="text"
+                className="w-full outline-none py-6 pr-4 font-medium placeholder:text-foreground"
+                placeholder="هر سوالی داری بپرس ..."
+              />
+              <div className="px-4">
+                <button
+                  type="button"
+                  className="size-10 rounded-full bg-gray-200 text-foreground flex items-center justify-center cursor-pointer"
+                >
+                  <ChevronLeft size={16} />
+                </button>
+              </div>
+            </div>
+
+            <span className="text-xs text-muted">
+              اگه نیاز به بررسی بیشتر باشه، وصل می‌شی به کارشناس پشتیبان.
+            </span>
           </div>
 
-          <span className="text-xs text-muted">
-            اگه نیاز به بررسی بیشتر باشه، وصل می‌شی به کارشناس پشتیبان.
-          </span>
+          <Stars
+            size={100}
+            className="absolute text-muted/40 top-1/2 left-1/5 -translate-y-1/2"
+          />
         </div>
-
-        <Stars
-          size={100}
-          className="absolute text-muted/40 top-1/2 left-1/5 -translate-y-1/2"
-        />
       </section>
 
       <section className="mb-10">
-        <div className="max-w-7xl mx-auto">
+        <div className="max-w-6xl mx-auto">
           <div className="relative text-center">
-            <span className="font-semibold text-xl bg-white px-4">
+            <span className="font-semibold text-xl text-black dark:text-white bg-background px-4">
               شاید برای شما هم سؤال باشد
             </span>
             <div className="-z-10 absolute top-1/2 -translate-y-1/2 w-full h-[0.5px] bg-gray-200"></div>
@@ -70,9 +72,9 @@ export function Faq() {
       </section>
 
       <section className="mb-10">
-        <div className="max-w-7xl mx-auto">
+        <div className="max-w-6xl mx-auto">
           <div className="relative text-center">
-            <span className="font-semibold text-xl bg-white px-4">
+            <span className="font-semibold text-xl text-black dark:text-white bg-background px-4">
               با دیوار بیشتر آشنا شوید
             </span>
             <div className="-z-10 absolute top-1/2 -translate-y-1/2 w-full h-[0.5px] bg-gray-200"></div>
@@ -92,7 +94,7 @@ export function Faq() {
                   </div>
                 </div>
 
-                <div className="flex items-center justify-center rounded-full group-hover:bg-gray-200 transition-colors duration-300 size-10">
+                <div className="flex items-center justify-center rounded-full dark:text-white group-hover:bg-gray-200 group-hover:text-black transition-colors duration-300 size-10">
                   <ChevronLeft size={16} />
                 </div>
               </div>
@@ -111,7 +113,7 @@ export function Faq() {
                   </div>
                 </div>
 
-                <div className="flex items-center justify-center rounded-full group-hover:bg-gray-200 transition-colors duration-300 size-10">
+                <div className="flex items-center justify-center rounded-full dark:text-white group-hover:bg-gray-200 group-hover:text-black transition-colors duration-300 size-10">
                   <ChevronLeft size={16} />
                 </div>
               </div>
@@ -130,7 +132,7 @@ export function Faq() {
                   </div>
                 </div>
 
-                <div className="flex items-center justify-center rounded-full group-hover:bg-gray-200 transition-colors duration-300 size-10">
+                <div className="flex items-center justify-center rounded-full dark:text-white group-hover:bg-gray-200 group-hover:text-black transition-colors duration-300 size-10">
                   <ChevronLeft size={16} />
                 </div>
               </div>
@@ -149,7 +151,7 @@ export function Faq() {
                   </div>
                 </div>
 
-                <div className="flex items-center justify-center rounded-full group-hover:bg-gray-200 transition-colors duration-300 size-10">
+                <div className="flex items-center justify-center rounded-full dark:text-white group-hover:bg-gray-200 group-hover:text-black transition-colors duration-300 size-10">
                   <ChevronLeft size={16} />
                 </div>
               </div>
@@ -159,9 +161,9 @@ export function Faq() {
       </section>
 
       <section className="mb-10">
-        <div className="max-w-7xl mx-auto">
+        <div className="max-w-6xl mx-auto">
           <div className="relative text-center">
-            <span className="font-semibold text-xl bg-white px-4">
+            <span className="font-semibold text-xl text-black dark:text-white bg-background px-4">
               پرتکرارترین سؤالات کاربران دیوار
             </span>
             <div className="-z-10 absolute top-1/2 -translate-y-1/2 w-full h-[0.5px] bg-gray-200"></div>
@@ -200,9 +202,9 @@ export function Faq() {
       </section>
 
       <section className="mb-10">
-        <div className="max-w-7xl mx-auto">
+        <div className="max-w-6xl mx-auto">
           <div className="relative text-center">
-            <span className="font-semibold text-xl bg-white px-4">
+            <span className="font-semibold text-xl text-black dark:text-white bg-background px-4">
               ارتباط با پشتیبانی دیوار
             </span>
             <div className="-z-10 absolute top-1/2 -translate-y-1/2 w-full h-[0.5px] bg-gray-200"></div>
@@ -219,7 +221,7 @@ export function Faq() {
 
               <div className="flex justify-center">
                 <Link to="#">
-                  <div className="text-white bg-primary hover:bg-primary/90 px-4 py-2 font-semibold rounded-md transition-colors duration-300">
+                  <div className="text-white bg-red-900 hover:bg-primary px-4 py-2 font-semibold rounded-md transition-colors duration-200">
                     شروع گفتگو آنلاین
                   </div>
                 </Link>
@@ -236,7 +238,7 @@ export function Faq() {
 
               <div className="flex justify-center">
                 <Link to="#">
-                  <div className=" text-primary border border-primary hover:text-white hover:bg-primary px-4 py-2 font-semibold rounded-md transition-colors duration-300">
+                  <div className=" text-primary border border-primary hover:text-white hover:bg-primary px-4 py-2 font-semibold rounded-md transition-colors duration-200">
                     شروع گفتگو آنلاین
                   </div>
                 </Link>
