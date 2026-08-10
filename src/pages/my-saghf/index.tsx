72e69d3e92f02c7dcbfab81f9a100acc44a1955c
flatrov <flatrov1@gmail.com>
2026-08-10 23:31:42 +0330

    feat: complete my-saghf page

--- src/pages/my-saghf/index.tsx
+++ src/pages/my-saghf/index.tsx
@@ -1,9 +1,14 @@
 import { Product } from "../../components/product";
 
 export function MySaghf() {
-  return <div className="grid grid-cols-3 gap-4">
-    <Product />
-    <Product />
-    <Product />
-  </div>;
+  return (
+    <>
+    <h6 className="text-xs font-medium mb-5 text-end">انواع آگهی‌ها و نیازمندی های کل ایران</h6>
+      <div className="grid grid-cols-3 gap-4">
+        <Product />
+        <Product />
+        <Product />
+      </div>
+    </>
+  );
 }
